fa30690cb0830dd024b73484b01e3dcabb3ef16f
Srimythili Padmanaban <132258905+Srimythili30@users.noreply.github.com>
2023-11-02 23:03:46 +0530

    Odd or Even.java

--- Day 1/Odd or Even.java
+++ Day 1/Odd or Even.java
@@ -1,1 +1,18 @@
-
+import java.util.Scanner;
+class OddEven
+{
+	public static void main(String[] args) {
+	    Scanner myObj = new Scanner(System.in);
+	    int num = myObj.nextInt();
+	    if (num%2==0){
+	        System.out.println("Even");
+	        
+	    }
+	    else{
+	        System.out.println("Odd");
+	        
+	    }
+	    
+		
+	}
+}
